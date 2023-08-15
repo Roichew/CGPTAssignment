d1d170f55cbe8a4c8b17a3ee111483984df4f13d
Roichew <roi_chew01@yahoo.com>
2023-08-15 10:19:48 +0800

    Merge branch 'Danial' into Roi

---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class footsteps : MonoBehaviour
+{
+    public AudioSource footstepsSound, sprintSound;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                footstepsSound.enabled = false;
+                sprintSound.enabled = true;
+            }
+            else
+            {
+                footstepsSound.enabled = true;
+                sprintSound.enabled = false;
+            }
+        }
+        else
+        {
+            footstepsSound.enabled = false;
+            sprintSound.enabled = false;
+        }
+    }
+}
+
