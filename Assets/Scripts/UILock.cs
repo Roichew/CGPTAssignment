438e2035ad6325ec202a2b3a380957f66f417297
DanzKhan22 <116078811+DanzKhan22@users.noreply.github.com>
2023-08-29 13:13:10 +0800

    lock

---
+++ Assets/Scripts/UILock.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UILock : MonoBehaviour
+{
+
+    public GameObject UIPuzzle;
+    public GameObject openText;
+    public GameObject GameObject;
+
+    public bool inReach;
+
+    void Start()
+    {
+        inReach = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            openText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            openText.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+
+        if (inReach && Input.GetButtonDown("Interact"))
+        {
+            
+            openText.SetActive(true);
+        }
+
+    }
+
+}
