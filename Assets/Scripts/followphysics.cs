607c7ccb6b98bb647a6bc89f0a738f2041cd1351
DanzKhan22 <116078811+DanzKhan22@users.noreply.github.com>
2023-09-09 16:02:38 +0800

    h

---
+++ Assets/Scripts/followphysics.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followphysics : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.MovePosition(target.transform.position);
+    }
+}
