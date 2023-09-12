7e6f315552b494d85c5485220c8e75c0090f1974
DanzKhan22 <116078811+DanzKhan22@users.noreply.github.com>
2023-09-13 02:49:43 +0800

    HUBHUHUBHUBHBHBHUHUHHHUBHHBUBUUUUB
    Hub duh

---
+++ Assets/Scripts/HubDoors.cs
@@ -0,0 +1,51 @@
+﻿using Codice.Client.BaseCommands;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HubDoors : MonoBehaviour
+{
+    public GameObject openText;
+    public AudioSource EnterLevel;
+    public bool inReach;
+    public string LevelOpen;
+
+
+    void Start()
+    {
+        inReach = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            openText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            openText.SetActive(false);
+        }
+    }
+
+
+
+    void Update()
+    {
+
+        if (inReach && Input.GetButtonDown("Interact"))
+        {
+            SceneManager.LoadScene(LevelOpen);
+            EnterLevel.Play();
+        }
+
+    }
+}
